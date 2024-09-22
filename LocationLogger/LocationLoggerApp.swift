a23ac88a2b4eeeb34adaedb8c101ebc3bb5e06c9
Yoshiyuki Koyanagi <moutend@gmail.com>
2024-09-22 16:41:54 +0900

    Create project

---
+++ LocationLogger/LocationLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationLoggerApp.swift
+//  LocationLogger
+//
+//  Created by Yoshiyuki Koyanagi on 2024/09/22.
+//
+
+import SwiftUI
+
+@main
+struct LocationLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
